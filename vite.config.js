f627d42414f6a0b64ed995c44c23151ba131312c
Srinivas Gowda <srinivas@solancer.com>
2024-09-29 02:30:43 +0530

    Update deps

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/stock-sim/",
+  base: "/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
